99da87d584316e0935151eba5a8b4e8ec50d9c6c
elemson <toluwanielemosho@gmail.com>
2024-06-27 00:34:46 +0300

    Enhance FileMetadata Model - Added @Getter and @AllArgsConstructor for boilerplate code reduction. - Included field-level annotations like @Id and @DateTimeFormat for appropriate data handling. - Implemented constructors and getter methods for all fields.

--- src/main/java/com/example/fileservice/model/FileMetadata.java
+++ src/main/java/com/example/fileservice/model/FileMetadata.java
@@ -1,13 +1,18 @@
 package com.example.fileservice.model;
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
+import org.springframework.format.annotation.DateTimeFormat;
 
 import java.util.Date;
 import java.util.Map;
 import java.util.UUID;
 
 @Document(collection = "files")
+@Getter
+@AllArgsConstructor
 public class FileMetadata {
 
     @Id
@@ -18,56 +23,9 @@ public class FileMetadata {
     private final Date createTime;
     private final Map<String, Object> meta;
     private final String source;
+
+    @DateTimeFormat(iso = DateTimeFormat.ISO.DATE_TIME)
     private final Date expireTime;
     private final String path;
     private final UUID token;
-
-    public FileMetadata(UUID id, String name, String contentType, long size, Date createTime, Map<String, Object> meta, String source, Date expireTime, String path, UUID token) {
-        this.id = id;
-        this.name = name;
-        this.contentType = contentType;
-        this.size = size;
-        this.createTime = createTime;
-        this.meta = meta;
-        this.source = source;
-        this.expireTime = expireTime;
-        this.path = path;
-        this.token = token;
-    }
-
-    public UUID getToken() {
-        return token;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public String getContentType() {
-        return contentType;
-    }
-
-    public long getSize() {
-        return size;
-    }
-
-    public Date getCreateTime() {
-        return createTime;
-    }
-
-    public Map<String, Object> getMeta() {
-        return meta;
-    }
-
-    public String getPath() {
-        return path;
-    }
-
-    public Date getExpireTime() {
-        return expireTime;
-    }
-
-    public String getSource() {
-        return source;
-    }
 }
