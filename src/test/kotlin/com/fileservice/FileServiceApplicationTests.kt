dce4458cf06cec12bb4390a9d7d3c40f8030f626
elemson <toluwanielemosho@gmail.com>
2024-07-01 01:17:30 +0300

    test: Add basic unit test for FileServiceApplication

---
+++ src/test/kotlin/com/fileservice/FileServiceApplicationTests.kt
@@ -0,0 +1,17 @@
+package com.fileservice
+
+import org.junit.jupiter.api.Assertions.assertTrue
+import org.junit.jupiter.api.Test
+
+/**
+ * Unit test for simple App.
+ */
+class FileServiceApplicationTests {
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    fun shouldAnswerWithTrue() {
+        assertTrue(true)
+    }
+}
