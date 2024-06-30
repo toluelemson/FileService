6ef8fcca1103ff426d5e4a7224b5905769879f6c
elemson <toluwanielemosho@gmail.com>
2024-07-01 01:17:13 +0300

    Update application name to FileServiceApplication for consistency

--- src/main/kotlin/com/example/fileservice/FileServiceApplication.kt
+++ src/main/kotlin/com/example/fileservice/FileServiceApplication.kt
@@ -1,18 +1,18 @@
 package com.example.fileservice
 
+import jakarta.annotation.PostConstruct
 import org.springframework.boot.SpringApplication
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.builder.SpringApplicationBuilder
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer
 import org.springframework.data.mongodb.repository.config.EnableMongoRepositories
 import java.util.TimeZone
-import javax.annotation.PostConstruct
 
 @SpringBootApplication
-@EnableMongoRepositories(basePackages = ["com.hrblizz.fileapi.data.repository"])
-class Application : SpringBootServletInitializer() {
+@EnableMongoRepositories(basePackages = ["com.example.fileservice.data.repository"])
+open class FileServiceApplication : SpringBootServletInitializer() {
     override fun configure(application: SpringApplicationBuilder): SpringApplicationBuilder {
-        return application.sources(Application::class.java)
+        return application.sources(FileServiceApplication::class.java)
     }
 
     @PostConstruct
@@ -23,7 +23,7 @@ class Application : SpringBootServletInitializer() {
     companion object {
         @JvmStatic
         fun main(args: Array<String>) {
-            SpringApplication.run(Application::class.java, *args)
+            SpringApplication.run(FileServiceApplication::class.java, *args)
         }
     }
 }
